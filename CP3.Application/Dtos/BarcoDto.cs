188532e81afe29e7d1711becec17860ae12635a0
Ezequiel Bispo de Jesus <ezequiel.bispo@kyndryl.com>
2024-11-09 22:38:48 -0300

    Atualização da classe BarcoDTO na camada Application.

--- CP3.Application/Dtos/BarcoDto.cs
+++ CP3.Application/Dtos/BarcoDto.cs
@@ -6,6 +6,11 @@ namespace CP3.Application.Dtos
     public class BarcoDto : IBarcoDto
     {
 
+        public int Id { get; set; }
+        public string Nome { get; set; }
+        public string Modelo { get; set; }
+        public int Ano { get; set; }
+        public double Tamanho { get; set; }
         public void Validate()
         {
 
