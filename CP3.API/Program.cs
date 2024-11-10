4ccd864993678376d58c92d68b656488b7434658
Ezequiel Bispo de Jesus <ezequiel.bispo@kyndryl.com>
2024-11-09 23:31:46 -0300

    String de conexão com o banco configurada e Injeção de dependências alterada.

--- CP3.API/Program.cs
+++ CP3.API/Program.cs
@@ -1,4 +1,9 @@
+using CP3.Application.Services;
+using CP3.Data.AppData;
+using CP3.Data.Repositories;
+using CP3.Domain.Interfaces;
 using CP3.IoC;
+using Microsoft.EntityFrameworkCore;
 
 var builder = WebApplication.CreateBuilder(args);
 
@@ -8,6 +13,10 @@ builder.Services.AddControllers();
 // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
+builder.Services.AddScoped<IBarcoRepository, BarcoRepository>();
+builder.Services.AddScoped<IBarcoApplicationService, BarcoApplicationService>();
+builder.Services.AddDbContext<ApplicationContext>(options =>
+    options.UseSqlServer(builder.Configuration.GetConnectionString("DefaultConnection")));
 
 var app = builder.Build();
 
